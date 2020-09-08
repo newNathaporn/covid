4912065c19c693464f507e92a153db85918619d2
newNathaporn <new_ziizyy@gmail.co.th>
2020-09-08 16:50:53 +0700

    add covid and fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for i in range(1,100):
+    if i % 3 == 0:
+        print "fizz"
+    elif i % 5 == 0:
+        print "buzz"
+    elif i % 3 == 0 && i % 5 ==0:
+        print "fizzbzz"
+    elif i % 3 != 0 && i % 5 != 0:
+        print i 
